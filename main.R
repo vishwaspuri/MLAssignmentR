0da994f6b90bc342fff6ca46dd9e49339fc063de
vishwaspuri <vishwaspuri09@gmail.com>
2020-04-11 11:58:54 +0530

    Bug fix-1

--- main.R
+++ main.R
@@ -9,9 +9,9 @@ set.seed(42)
 #Exploring data
 #----------------------------------------------------------
 print("SUMMARY OF DATASET")
-summary(flowerData)
-print(head(flowerData))
-print(tail(flowerData))
+#summary(flowerData)
+#print(head(flowerData))
+#print(tail(flowerData))
 #----------------------------------------------------------
 #Data Visualization and data is stored in 'plot.pdf' file in the working directory
 #----------------------------------------------------------
@@ -57,14 +57,14 @@ testingDataset<-shuffledFlowerData[113:150,]
 #Classification without dimensionality reduction
 #----------------------------------------------------------
 fit<-ksvm(Species~., data=trainingDataset)
-summary(fit)
+#summary(fit)
 prediction<-predict(fit, testingDataset, type='response')
 print("The accuracy of the SVM is as shown below:")
 print(accuracy(prediction,testingDataset[,5]))
 #----------------------------------------------------------
-print('Principal Component Analysis and classification')
+#print('Principal Component Analysis and classification')
 #----------------------------------------------------------
-shuffledFlowerData.pca.eigenvalues<-princomp(x=shuffledFlowerData[,1:4], cors=TRUE, scores = TRUE)
+shuffledFlowerData.pca.eigenvalues<-princomp(x=shuffledFlowerData[,1:4], scores = TRUE)
 shuffledFlowerData.pca.eigenvectors<-shuffledFlowerData.pca.eigenvalues$scores
 plot(shuffledFlowerData.pca.eigenvectors[,1], col=c("red","blue","green"), ylab = "PCA component 1")
 mostUsefulData<-shuffledFlowerData.pca.eigenvectors[,1]
@@ -75,7 +75,7 @@ postPCADataset<-cbind(mostUsefulData, shuffledFlowerData['Species'])
 #----------------------------------------------------------
 #traininng data
 trainingDataset.pca<-postPCADataset[1:trainingDatasetSize,]
-print(trainingDataset.pca)
+#print(trainingDataset.pca)
 #testing data
 testingDataset.pca<-postPCADataset[113:150,]
 #----------------------------------------------------------
@@ -84,7 +84,7 @@ testingDataset.pca<-postPCADataset[113:150,]
 fit.pca<-ksvm(Species~., data=trainingDataset.pca)
 #summary(fit.pca)
 prediction.pca<-predict(fit.pca, testingDataset.pca, type='response')
-print("The accuracy of the SVM is as shown below:")
+print("The accuracy of the SVM after PCA is as shown below:")
 print(accuracy(prediction.pca,testingDataset.pca[,2]))
 #----------------------------------------------------------
 #Fischers Linear Discriminant and classification
